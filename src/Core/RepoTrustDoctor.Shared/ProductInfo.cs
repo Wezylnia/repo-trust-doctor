b5b3f1110f247fb5395f24714530a7cda50ca2b9
Serhat Har <serhatrah83@gmail.com>
2026-06-03 18:45:16 +0300

    cli: centralize release version

---
+++ src/Core/RepoTrustDoctor.Shared/ProductInfo.cs
@@ -0,0 +1,8 @@
+namespace RepoTrustDoctor.Shared;
+
+public static class ProductInfo
+{
+    public const string Name = "Repository Trust Doctor";
+    public const string CommandName = "repo-trust-doctor";
+    public const string Version = "0.1.1-alpha";
+}
